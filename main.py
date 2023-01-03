3748344400b5e7a3a92de874dfa23b2f66f371d1
Sanqn <Sandisvert@gmail.com>
2023-01-03 22:45:36 +0300

    put_query

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ from typing import Union
 
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 
 @app.get('/')
@@ -14,6 +21,11 @@ async def main():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def read_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item_name": item.name, "item_price": item.price}
 
 
 if __name__ == '__main__':
