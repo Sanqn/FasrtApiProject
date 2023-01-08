6f383bdc7f843e20aad9971438ca8a46b7f11e66
Sanqn <Sandisvert@gmail.com>
2023-01-08 19:14:43 +0300

    put_query

--- main.py
+++ main.py
@@ -28,15 +28,19 @@ async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
+@app.put("/read_item/{item_id}")
 async def read_item(item_id: int, item: Item):
     return {"item_id": item_id, "item_name": item.name, "item_price": item.price}
 
 
-@app.post("/post_mes/{post_message}")
+@app.post("/post_mes/{post_message}")  # http://127.0.0.1:8000/post_mes/Wow i found you
 async def post_message(post_message: str):
     print(post_message)
     return {"post_message": post_message}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
 
 
 if __name__ == '__main__':
